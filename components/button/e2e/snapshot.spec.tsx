99164354161623f9540f5b118ac97fef22e86954
Bob Yu <2455045365@qq.com>
2021-09-20 12:04:19 +0800

    feat:e2e端到端测试

---
+++ components/button/e2e/snapshot.spec.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
+import Button from '..';
+import 'jest-environment-puppeteer';
+const toMatchSnapshot = configureToMatchImageSnapshot({
+  customSnapshotsDir: `${process.cwd()}/snapshots`,
+  customDiffDir: `${process.cwd()}/diffSnapshots`,
+});
+expect.extend({ toMatchSnapshot });
+describe('Button snapshot', () => {
+  it('screenshot should correct', async () => {
+    await jestPuppeteer.resetPage();
+    await page.goto(`file://${process.cwd()}/tests/index.html`);
+    const html = ReactDOMServer.renderToString(<Button>按钮</Button>);
+    await page.evaluate((innerHTML:string) => {
+      document.querySelector('#root')!.innerHTML = innerHTML;
+    }, html);
+    const screenshot = await page.screenshot(); // 生成一张新的快照
+    expect(screenshot).toMatchSnapshot(); // 比较新的快照和老的快照是否相同
+  });
+});
